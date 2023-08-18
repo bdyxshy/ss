b06105e021c29916044118843b90249342bc2f3b
Shi Yu <4763852@qq.com>
2023-08-19 00:03:01 +0800

    Update fomz.js

--- fomz.js
+++ fomz.js
@@ -4,6 +4,4 @@
 
 *******************************/
 
-body = $response.body.replace(/\"isValid":\d+/g, '\"isValid":1')
-body = body.replace(/\"expiredTs":\d+/g, '\"expiredTs":4102415999')
-$done({body});
+var youxiu = JSON.parse($response.body);youxiu.data.isValid = 1;youxiu.data.expiredTs = 4102415999;$done({body : JSON.stringify(youxiu)});
